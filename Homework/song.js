5ed6bed3d3a95ebc14667ad7cb27b4f1eed70d8a
Amila De Silva <amilad@99x.lk>
2019-01-21 14:03:28 +0530

    Added comments

--- Homework/song.js
+++ Homework/song.js
@@ -1,7 +1,16 @@
+//Title of the song
 var title = "Fix you";
+
+//Artist
 var artist = "Coldplay";
+
+//Album
 var album = "X&Y";
+
+//Genre that the song belongs to
 var genre = "Alternative";
+
+//Run time in seconds
 var durationInSeconds  = "294";
 
 console.log(title);
